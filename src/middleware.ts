98dbe0aacacf4c32285a98d1afb806a1a76d0cbc
Jordan Riser <riser.jordan@gmail.com>
2023-07-05 09:58:27 -0400

    removes middleware to create user

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,6 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      console.log("beep 1");
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // create user in DB
-    return fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
-      method: "POST",
-      body: JSON.stringify({
-        id: auth.userId as string,
-        name: auth.user?.emailAddresses?.[0].emailAddress || "",
-      }),
-    })
-      .then(() => {
-        return NextResponse.next();
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("beep 3");
-        return redirectToSignIn({ returnBackUrl: req.url });
-      });
-  },
-  publicRoutes: ["/:locale/sign-in", "/:locale/sign-up"],
-});
+export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
